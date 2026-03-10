22e1be5f19e6a184c9a483a5135a2203ab7fcfbc
J0EYJ0ES <garuj10@outlook.com>
2026-03-10 15:25:19 -0700

    Updated GameManager script
    made animals appear by pressing S,  randomized animal spawn

--- P7BasicGamePlayJimenez/Assets/Scripts/SpawnManager.cs
+++ P7BasicGamePlayJimenez/Assets/Scripts/SpawnManager.cs
@@ -1,8 +1,11 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +16,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Randomly generate animal index and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
